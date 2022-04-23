18610fbdb44ff0321a3b981bbccffea9d1a26f84
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2022-04-23 21:22:12 +0430

    ADD: Book service structure added.

---
+++ book/src/test/java/com/nimaebrazi/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimaebrazi.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
